6007e1d95e0587b939c0d6e5bb9e15d901e65f69
Jonathan Bowen <ew15dro6k216@opayq.net>
2019-12-18 18:42:08 -0500

    Create repo

---
+++ ex01/ex01e.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* This is a comment. */
+int main(int argc, char *argv[])
+{
+    int distance = 100;
+    int money = 5;
+
+    // this is also a comment
+    printf("You are %d miles away.\nMore text.\nThird line.\n"
+          "Another line with second variable %d and %d.\n", distance, 12, money);
+
+    return 0;
+}
