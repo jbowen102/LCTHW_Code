0e8c3a023bb19e3e0acdea21e7d631e93040af29
Jonathan Bowen <ew15dro6k216@opayq.net>
2020-01-10 22:19:10 -0500

    Finish ex08

--- ex08/ex08.c
+++ ex08/ex08.c
@@ -5,14 +5,21 @@ int main(int argc, char *argv[])
 	int i = 0;
 
 	if (argc == 1) {
+		printf("You have no arguments.\n");
+	}
+	else if (argc == 2) {
 		printf("You only have one argument. You suck.\n");
-	} else if (argc > 1 && argc < 4) {
+	} else if (argc > 2 && argc < 5) {
 		printf("Here's your arguments:\n");
 
-		for (i=0; i < argc; i++) {
+		for (i=1; i < argc; i++) {
 			printf("%s ", argv[i]);
 		}
 		printf("\n");
+	// } else if (argc == 5) {
+	// 	printf("Four arguments.\n");
+	// } else if (1) {
+	// 	printf("text\n");
 	} else {
 		printf("You have too many arguments. You suck.\n");
 	}
